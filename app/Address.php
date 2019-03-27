4e51060cb91fed15cb32cce289c48555ea23d8b8
Kevin Ruscoe <hello@kevinruscoe.me>
2019-03-27 20:42:10 +0000

    clean up

--- app/Address.php
+++ app/Address.php
@@ -3,36 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use KevinRuscoe\Traits\PointParserTrait;
 
 class Address extends Model
 {
+    use PointParserTrait;
+
     protected $fillable = [
         'location'
     ];
-
-    public function setLocationAttribute($value)
-    {
-        if (is_array($value)) {
-            if (count($value) !== 2) {
-                throw new \Exception("An arrayed location needs 2 values");
-            }
-
-            $lat = isset($value['lat']) ? $value['lat'] : $value[0];
-            $lng = isset($value['lng']) ? $value['lng'] : $value[1];
-
-            $value = sprintf("POINT(%s, %s)", $lat, $lng);
-        }
-
-        $this->attributes['location'] = \DB::raw($value);
-    }
-
-    public function getLocationAttribute($value)
-    {
-        $unpacked = unpack('x/x/x/x/corder/Ltype/dlat/dlon', $value);
-        
-        return [
-            'lat' => $unpacked['lat'],
-            'lng' => $unpacked['lon'],
-        ];
-    }
 }
